4d94677664446a7434c2889eef48bdf996423d9b
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-03-26 23:34:08 +0900

    update: function of finding user
    change from returning void to return returning user

--- api/app/Http/Repositories/UserRepository.php
+++ api/app/Http/Repositories/UserRepository.php
@@ -8,13 +8,11 @@ class UserRepository {
      * find user on database function
      *
      * @param array $params
-     * @return void
+     * @return User
      */
-    public function findUser(array $params){
+    public function findUser(array $params): User
+    {
         return User::where($params[0], $params[1])->first();
     }
-
-    
-
 
 }
