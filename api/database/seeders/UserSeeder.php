d727605874d555233df518804878ca9b8b0b4906
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-03-26 18:20:26 +0900

    create: user seeder

---
+++ api/database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'test_taro',
+            'password' => Hash::make('Password1'),
+            'email' => 'example@example.com',
+        ]);
+        User::factory()->count(10)->create();
+    }
+}
