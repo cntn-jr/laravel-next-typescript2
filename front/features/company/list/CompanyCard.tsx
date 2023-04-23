b1d3395cc568475c49eaa0b625ad4b83e915ed69
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-04-23 15:37:38 +0900

    update: add card action to company card

--- front/features/company/list/CompanyCard.tsx
+++ front/features/company/list/CompanyCard.tsx
@@ -1,28 +1,29 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
 
 export default function CompanyCard() {
     return (
         <Card
             sx={{
                 height: "250px",
-                border: "3px solid #ccc",
-                cursor: "pointer",
+                border: "2px solid #ccc",
             }}
         >
-            <CardContent>
-                <Typography variant="h5" component="div" noWrap={true}>
-                    株式会社ノースディテール
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    北海道
-                </Typography>
-                <Typography color="text.secondary">特徴</Typography>
-                <Typography noWrap={true}>・IT</Typography>
-                <Typography noWrap={true}>・Webアプリ作成</Typography>
-                <Typography noWrap={true}>・PHP</Typography>
-                <Typography noWrap={true}>・フレックスタイム制</Typography>
-                <Typography noWrap={true}>・自社開発</Typography>
-            </CardContent>
+            <CardActionArea>
+                <CardContent>
+                    <Typography variant="h5" component="div" noWrap={true}>
+                        株式会社ノースディテール
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        北海道
+                    </Typography>
+                    <Typography color="text.secondary">特徴</Typography>
+                    <Typography noWrap={true}>・IT</Typography>
+                    <Typography noWrap={true}>・Webアプリ作成</Typography>
+                    <Typography noWrap={true}>・PHP</Typography>
+                    <Typography noWrap={true}>・フレックスタイム制</Typography>
+                    <Typography noWrap={true}>・自社開発</Typography>
+                </CardContent>
+            </CardActionArea>
         </Card>
     );
 }
