9ded0435fdb20218d70ee125d4995d38f3568f59
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-04-25 23:25:28 +0900

    update: install react-router-dom

--- front/hooks/useAuthenticate.ts
+++ front/hooks/useAuthenticate.ts
@@ -1,12 +1,16 @@
 import { AuthenticateApi } from "@/pages/api/AuthenticateApi";
+import { useCookies } from "react-cookie";
 import { useMutation } from "react-query";
 
 export const useAuthenticate = () => {
+    const navigate = useNavigate();
+    const [cookies, setCookie] = useCookies(['isAuth']);
     const { login } = AuthenticateApi();
     const loginMutation = useMutation(login, {
         onError: () => {},
         onSuccess: (data) => {
             console.log(data);
+            setCookie("isAuth", true);
         },
         onSettled: () => {},
     });
---
+++ front/types/CookieType.ts
@@ -0,0 +1,3 @@
+export type CookieType = {
+    isAuth: boolean;
+};
