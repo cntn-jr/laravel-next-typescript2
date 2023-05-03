5d13fcd881160d841d934700f4dc6dcf9891ab8c
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-05-03 10:40:24 +0900

    add: 会社データとそれに紐づく特徴データを取得する関数

--- api/app/Models/Company.php
+++ api/app/Models/Company.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Company extends Model
 {
     use HasFactory;
+
+    /**
+     * Get all of the comments for the Company
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function features(): HasMany
+    {
+        return $this->hasMany(CompanyFeature::class, 'company_id', 'id');
+    }
 }
