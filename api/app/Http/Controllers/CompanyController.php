d9158c1f17e43bdb1c39a899203eb6970ab03d86
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-05-03 10:53:37 +0900

    add: 会社情報とそれに紐づく特徴データを取得する会社コントローラー内の関数の作成

--- api/app/Http/Controllers/CompanyController.php
+++ api/app/Http/Controllers/CompanyController.php
@@ -2,6 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\CompanyService;
+
 class CompanyController extends Controller
 {
+    public function __construct(private ?CompanyService $_companyService = null)
+    {
+    }
+
+    public function index()
+    {
+        if (!$companies = $this->_companyService->getCompanies()) {
+            return response()->json([], 401);
+        }
+        return response()->json($companies, 200);
+    }
 }
